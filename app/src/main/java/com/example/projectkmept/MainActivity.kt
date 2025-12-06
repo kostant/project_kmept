cdad9e2619f3c378bc34eb1cfbea6641247e41f9
Студент <student@imes.local>
2025-12-06 12:46:33 +0300

    initial

---
+++ app/src/main/java/com/example/projectkmept/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.projectkmept
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projectkmept.screens.AntonchikActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.antonchikButton).setOnClickListener {
+            val intent = Intent(this, AntonchikActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/projectkmept/screens/AntonchikActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projectkmept.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectkmept.R
+
+class AntonchikActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_antonchik)
+    }
+}
