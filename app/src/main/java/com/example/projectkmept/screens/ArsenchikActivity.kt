d4042dbb632cad00e016e320f39eb28643ab757a
Студент <student@imes.local>
2025-12-20 14:34:40 +0300

    arsensigma666

--- app/src/main/java/com/example/projectkmept/screens/ArsenchikActivity.kt
+++ app/src/main/java/com/example/projectkmept/screens/ArsenchikActivity.kt
@@ -2,11 +2,20 @@ package com.example.projectkmept.screens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.projectkmept.R
+import com.example.projectkmept.oorzhak.ArsenchikAdapter
 
 class ArsenchikActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.oorzhak)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val adapter = ArsenchikAdapter()
+        recyclerView.adapter = adapter
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
