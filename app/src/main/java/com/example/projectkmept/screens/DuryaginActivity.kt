5b6f28755a092f54c6913d41a50f8756f7347f62
Студент <student@imes.local>
2025-12-20 14:55:40 +0300

    123123

--- app/src/main/java/com/example/projectkmept/screens/DuryaginActivity.kt
+++ app/src/main/java/com/example/projectkmept/screens/DuryaginActivity.kt
@@ -1,4 +1,23 @@
-package com.example.projectkmept.screens
+package com.example.projectkmept.duryagin
 
-class DuryaginActivity {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectkmept.R
+import com.example.projectkmept.duryagin.duryagin.DuryaginViewHolder
+
+class ArsenchikAdapter: RecyclerView.Adapter<DuryaginViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DuryaginViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.item_duryagin, parent, false)
+        return DuryaginViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onBindViewHolder(holder: DuryaginViewHolder, position: Int) {
+
+    }
 }
